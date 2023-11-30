42b5a9765966e6ca9e7acb5f0110f4f413459b9b
DarkADR <DarkADR@DESKTOP-1STC0KU>
2023-11-30 19:44:39 +0700

    Add project files.

---
+++ MyOPTraits.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+// I love Indomie Instant Noodles
+namespace My_OP_Traits
+{
+    // Class one, set up the mod
+    public class MyOPTraits : Mod
+    {
+        public MyOPTraits(ModContentPack content) : base(content)
+        {
+            new Harmony("MyOPTraits.main").PatchAll();
+        }
+    }
+    
+    // Class two, make it easier to get at your special Defs, you could look it up by name and skip this step but this is probably nicer code
+    [DefOf]
+    public static class MyOPTraitsDefOf
+    {
+        public static TraitDef My_OP_Traits_GodTrait;
+
+        static MyOPTraitsDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(MyOPTraitsDefOf));
+    }
+
+    // Class three, the actual patch
+    [HarmonyPatch(typeof(HediffSet), nameof(HediffSet.AddDirect))]
+    public static class HediffSet_AddDirectPatch
+    {
+        [HarmonyPrefix]
+        public static bool AddDirect(HediffSet __instance, Hediff hediff)
+        {
+            return !hediff.def.isBad || !__instance.pawn.story.traits.HasTrait(MyOPTraitsDefOf.My_OP_Traits_GodTrait);
+        }
+    }
+}
